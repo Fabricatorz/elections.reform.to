25a0c134000b845271f79e9498da55cf631b6cce
Christopher Adams <chris@raysend.com>
2014-08-07 16:58:08 +0800

    Break up queries to be easier to read

--- lib/records.rb
+++ lib/records.rb
@@ -3,19 +3,59 @@ require 'active_record'
 class CandidatesRecords
 
   def self.by_id(cycle, id)
-      ActiveRecord::Base.connection.select_one("SELECT * FROM candidates WHERE can_id = #{ActiveRecord::Base.sanitize(id)} AND election_yr = #{ActiveRecord::Base.sanitize(cycle)}")
+    s_cycle = ActiveRecord::Base.sanitize(cycle)
+    s_id = ActiveRecord::Base.sanitize(id)
+
+    selection = "can_id = #{s_id} AND election_yr = #{s_cycle}"
+
+    relation = "SELECT #{projection} FROM #{relations} WHERE #{selection}"
+    ActiveRecord::Base.connection.select_one(relation)
   end
 
   def self.by_state(cycle, state)
-    ActiveRecord::Base.connection.select_all("SELECT * FROM candidates WHERE can_off_sta = #{ActiveRecord::Base.sanitize(state)} AND election_yr = #{ActiveRecord::Base.sanitize(cycle)} ORDER BY can_off_sta ASC, can_off_dis ASC, can_nam ASC")
+    s_state = ActiveRecord::Base.sanitize(state)
+    s_cycle = ActiveRecord::Base.sanitize(cycle)
+
+    selection = "can_off_sta = #{s_state} AND election_yr = #{s_cycle}"
+
+    relation = "SELECT #{projection} FROM #{relations} WHERE #{selection} ORDER BY #{order}"
+    ActiveRecord::Base.connection.select_all(relation)
   end
 
   def self.by_office(cycle, state, office)
-    @collection = ActiveRecord::Base.connection.select_all("SELECT * FROM candidates WHERE can_off_sta = #{ActiveRecord::Base.sanitize(state)} AND can_off = #{ActiveRecord::Base.sanitize(office)} AND election_yr = #{ActiveRecord::Base.sanitize(cycle)} ORDER BY can_off_sta ASC, can_off_dis ASC, can_nam ASC")
+    s_office = ActiveRecord::Base.sanitize(office)
+    s_state = ActiveRecord::Base.sanitize(state)
+    s_cycle = ActiveRecord::Base.sanitize(cycle)
+
+    selection = "can_off = #{s_office} AND can_off_sta = #{s_state} AND election_yr = #{s_cycle}"
+
+    relation = "SELECT #{projection} FROM #{relations} WHERE #{selection} ORDER BY #{order}"
+    ActiveRecord::Base.connection.select_all(relation)
   end
 
   def self.by_district(cycle, state, district, office = 'H')
-    ActiveRecord::Base.connection.select_all("SELECT * FROM candidates WHERE can_off_sta = #{ActiveRecord::Base.sanitize(state)} AND can_off = #{ActiveRecord::Base.sanitize(office)} AND can_off_dis = #{ActiveRecord::Base.sanitize(district)} AND election_yr = #{ActiveRecord::Base.sanitize(cycle)} ORDER BY can_off_sta ASC, can_off_dis ASC, can_nam ASC")
+    s_office = ActiveRecord::Base.sanitize(office)
+    s_district = ActiveRecord::Base.sanitize(district)
+    s_state = ActiveRecord::Base.sanitize(state)
+    s_cycle = ActiveRecord::Base.sanitize(cycle)
+
+    selection = "can_off = #{s_office} AND can_off_sta = #{s_state} AND can_off_dis = #{s_district} AND election_yr = #{s_cycle}"
+
+    relation = "SELECT #{projection} FROM #{relations} WHERE #{selection} ORDER BY #{order}"
+    ActiveRecord::Base.connection.select_all(relation)
+  end
+
+  protected
+  def self.projection
+    "can_id, can_nam, can_off, can_off_sta, can_off_dis, can_par_aff"
+  end
+
+  def self.relations
+    "candidates"
+  end
+
+  def self.order
+    "can_off_sta ASC, can_off_dis ASC, can_nam ASC"
   end
 end
 
