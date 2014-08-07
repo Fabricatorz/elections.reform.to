f3af117ff3cd976168552c9d0bcc0f94dcdbe7ac
Christopher Adams <chris@raysend.com>
2014-08-07 18:03:10 +0800

    Refactor resources into hierarchy

--- app.rb
+++ app.rb
@@ -20,94 +20,60 @@ class JsonResource < Webmachine::Resource
   end
 end
 
-class CandidateResource < JsonResource
-  def allowed_methods
-    ["GET"]
-  end
+class ElectionsResource < JsonResource
 
   def cycle
     request.path_info[:cycle].to_i
   end
 
-  def id
-    # Remove any .json extension from the token
-    request.path_info[:id].sub(/\.json$/,'')
+  def error_body
+    '{"status":"ERROR","copyright":"Copyright (c) 2014 Fabricatorz, LLC. All Rights Reserved.","errors":["Record not found"]}'
   end
 
-  def href
-    @request.base_uri.to_s + + "finances/#{cycle}/candidates/#{id}"
-  end
-
-  def base_uri
-    @request.base_uri.to_s + "finances/#{cycle}/"
-  end
-
-  def resource_exists?
-    @item = CandidatesRecords.by_id(cycle, id)
-
-    if @item.nil?
-      @response.headers['Content-Type'] = 'application/json'
-      @response.body = '{"status":"ERROR","copyright":"Copyright (c) 2014 Fabricatorz, LLC. All Rights Reserved.","errors":["Record not found"]}'
-    end
-
-    !@item.nil?
-  end
-
-  def to_json
-    CandidateSerializer.new(base_uri, cycle, @item).to_json
-  end
 end
 
-class SeatsResource < JsonResource
-  def allowed_methods
-    ["GET"]
-  end
+class StatesResource < ElectionsResource
 
-  def cycle
-    request.path_info[:cycle].to_i
-  end
-
-  def state_token
+  def state
     # Remove any .json extension from the token
     request.path_info[:state].sub(/\.json$/,'')
   end
 
-  def base_uri
-    @request.base_uri.to_s + "finances/#{cycle}/"
-  end
-
-  def resource_exists?
-    @collection = CandidatesRecords.by_state(cycle, state_token)
-
-    unless @collection.any?
-      @response.headers['Content-Type'] = 'application/json'
-      @response.body = '{"status":"ERROR","copyright":"Copyright (c) 2014 Fabricatorz, LLC. All Rights Reserved.","errors":["Record not found"]}'
+  def finish_request
+    if defined? @resources
+      unless @resources.any?
+        response.headers['Content-Type'] = 'application/json'
+        response.body = error_body
+      end
     end
-
-    @collection.any?
+    nil
   end
 
-  def to_json
-    CandidatesSerializer.new(base_uri, cycle, state_token, nil, @collection).to_json
-  end
 end
 
-class OfficeSeatsResource < JsonResource
+class OfficesResource < StatesResource
+
+  def office
+    request.path_info[:office].sub(/\.json$/,'')
+  end
+
+end
+
+class DistrictsResource < OfficesResource
+
+  def district
+    request.path_info[:district].sub(/\.json$/,'')
+  end
+
+end
+
+class CandidateResource < ElectionsResource
   def allowed_methods
     ["GET"]
   end
 
-  def cycle
-    request.path_info[:cycle].to_i
-  end
-
-  def state_token
-    request.path_info[:state]
-  end
-
-  def office_token
-    # Remove any .json extension from the token
-    request.path_info[:office].sub(/\.json$/,'')
+  def id
+    request.path_info[:id].sub(/\.json$/,'')
   end
 
   def base_uri
@@ -115,48 +81,74 @@ class OfficeSeatsResource < JsonResource
   end
 
   def resource_exists?
-    case office_token
+    @resource = CandidatesRecords.by_id(cycle, id)
+
+    !@resource.nil?
+  end
+
+  def to_json
+    CandidateSerializer.new(base_uri, cycle, @resource).to_json
+  end
+
+  def finish_request
+    if @resource.nil?
+      response.headers['Content-Type'] = 'application/json'
+      response.body = error_body
+    end
+  end
+
+end
+
+class StateResource < StatesResource
+  def allowed_methods
+    ["GET"]
+  end
+
+  def base_uri
+    @request.base_uri.to_s + "finances/#{cycle}/"
+  end
+
+  def resource_exists?
+    @resources = CandidatesRecords.by_state(cycle, state)
+
+    @resources.any?
+  end
+
+  def to_json
+    CandidatesSerializer.new(base_uri, cycle, state, nil, @resources).to_json
+  end
+end
+
+class OfficeResource < OfficesResource
+  def allowed_methods
+    ["GET"]
+  end
+
+  def base_uri
+    @request.base_uri.to_s + "finances/#{cycle}/"
+  end
+
+  def resource_exists?
+    case office
     when 'house'
       can_off = 'H'
     when 'senate'
       can_off = 'S'
     end
 
-    @collection = CandidatesRecords.by_office(cycle, state_token, can_off)
+    @resources = CandidatesRecords.by_office(cycle, state, can_off)
 
-    unless @collection.any?
-      @response.headers['Content-Type'] = 'application/json'
-      @response.body = '{"status":"ERROR","copyright":"Copyright (c) 2014 Fabricatorz, LLC. All Rights Reserved.","errors":["Record not found"]}'
-    end
-
-    @collection.any?
+    @resources.any?
   end
 
   def to_json
-    CandidatesSerializer.new(base_uri, cycle, state_token, nil, @collection).to_json
+    CandidatesSerializer.new(base_uri, cycle, state, nil, @resources).to_json
   end
 end
 
-class DistrictSeatsResource < JsonResource
+class DistrictResource < DistrictsResource
   def allowed_methods
     ["GET"]
-  end
-
-  def cycle
-    request.path_info[:cycle].to_i
-  end
-
-  def state_token
-    request.path_info[:state]
-  end
-
-  def office_token
-    request.path_info[:office]
-  end
-
-  def district_token
-    # Remove any .json extension from the token
-    request.path_info[:district].sub(/\.json$/,'')
   end
 
   def base_uri
@@ -164,18 +156,13 @@ class DistrictSeatsResource < JsonResource
   end
 
   def resource_exists?
-    @collection = CandidatesRecords.by_district(cycle, state_token, district_token)
+    @resources = CandidatesRecords.by_district(cycle, state, district)
 
-    unless @collection.any?
-      @response.headers['Content-Type'] = 'application/json'
-      @response.body = '{"status":"ERROR","copyright":"Copyright (c) 2014 Fabricatorz, LLC. All Rights Reserved.","errors":["Record not found"]}'
-    end
-
-    @collection.any?
+    @resources.any?
   end
 
   def to_json
-    CandidatesSerializer.new(base_uri, cycle, state_token, district_token, @collection).to_json
+    CandidatesSerializer.new(base_uri, cycle, state, district, @resources).to_json
   end
 end
 
@@ -185,9 +172,9 @@ App = Webmachine::Application.new do |app|
   end
   app.routes do
     add ["finances", :cycle, "candidates", :id], CandidateResource
-    add ["finances", :cycle, "seats", :state], SeatsResource
-    add ["finances", :cycle, "seats", :state, :office], OfficeSeatsResource
-    add ["finances", :cycle, "seats", :state, "house", :district], DistrictSeatsResource
+    add ["finances", :cycle, "seats", :state], StateResource
+    add ["finances", :cycle, "seats", :state, :office], OfficeResource
+    add ["finances", :cycle, "seats", :state, "house", :district], DistrictResource
     #add ['trace', '*'], Webmachine::Trace::TraceResource
   end
 end
